e435e7ebab86a72befb12b243f2dee64b65916df
Kyunghwan Kwon <kwon@toanyone.net>
2021-03-25 09:10:44 +0900

    Add test framework, cpputest

---
+++ tests/runners/example.mk
@@ -0,0 +1,8 @@
+COMPONENT_NAME = example
+
+SRC_FILES = ../src/main.c
+
+TEST_SRC_FILES = \
+	src/example_test.cpp
+
+include MakefileRunner.mk
---
+++ tests/src/example_test.cpp
@@ -0,0 +1,11 @@
+#include "CppUTest/TestHarness.h"
+
+TEST_GROUP(example) {
+	void setup(void) {
+	}
+	void teardown(void) {
+	}
+};
+
+TEST(example, t) {
+}
