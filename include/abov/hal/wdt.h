1d1922ed87d559c499373b130f22f7c236a45425
Kyunghwan Kwon <kwon@toanyone.net>
2021-04-03 16:25:41 +0900

    Fix missing header

---
+++ include/abov/hal/wdt.h
@@ -0,0 +1,32 @@
+#ifndef ABOV_WDT_HAL_H
+#define ABOV_WDT_HAL_H
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <stdbool.h>
+
+/**
+ * Reset WDT unit
+ *
+ * This function makes WDT unit the reset state.
+ */
+void wdt_reset(void);
+
+void wdt_set_prescaler(uint32_t div_factor);
+void wdt_reload(uint32_t timeout);
+void wdt_start(void);
+void wdt_stop(void);
+void wdt_set_debug_hold_mode(bool enable);
+bool wdt_is_event_raised(void);
+void wdt_set_interrupt(bool enable);
+uint32_t wdt_get_count(void);
+uint32_t wdt_get_timeout(void);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* ABOV_WDT_HAL_H */
