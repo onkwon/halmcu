9cb325ade03fdda8418b773b7cea614672be8723
Kyunghwan Kwon <kwon@toanyone.net>
2021-04-09 14:12:24 +0900

    example: add systick example

--- docs/source/api/systick.rst
+++ docs/source/api/systick.rst
@@ -3,4 +3,11 @@ SysTick
 
 `SYSTICK_PRESCALER`
 
+Examples
+--------
+.. literalinclude:: ../../../examples/systick/src/main.c
+   :language: C
+
+API
+---
 .. c:autodoc:: ../../include/abov/asm/arm/systick.h
---
+++ examples/systick/src/main.c
@@ -0,0 +1,27 @@
+#include "abov/asm/arm/systick.h"
+#include "abov/hal/irq.h"
+#include "abov/hal/gpio.h"
+
+#define LED_PIN			(GPIOD + 1)
+
+int main(void)
+{
+	gpio_open(LED_PIN, GPIO_MODE_PUSHPULL);
+
+	systick_set_frequency(1);
+	systick_clear();
+	systick_start();
+
+	while (1) {
+		/* hang */
+	}
+
+	return 0;
+}
+
+void ISR_systick(void)
+{
+	static int led = 0;
+	led ^= 1;
+	gpio_write(LED_PIN, led);
+}
