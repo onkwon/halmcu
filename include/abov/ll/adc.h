556c2d26765e31d51533fb0584514cb90f07e7e6
Kyunghwan Kwon <kwon@toanyone.net>
2021-04-15 21:07:48 +0900

    Add adc docs

--- include/abov/ll/adc.h
+++ include/abov/ll/adc.h
@@ -54,21 +54,57 @@ typedef enum {
 	ADC_EVENT_MASK			= (3U),
 } adc_event_t;
 
+/**
+ * Reset ADC
+ *
+ * This function makes the given ADC the reset default state.
+ *
+ * :param adc: a peripheral enumerated in :c:type:`peripheral_t`
+ */
 void adc_reset(peripheral_t adc);
+/**
+ * Enable the ADC
+ *
+ * :param adc: a peripheral enumerated in :c:type:`peripheral_t`
+ */
 void adc_enable(peripheral_t adc);
+/**
+ * Disable the ADC
+ *
+ * :param adc: a peripheral enumerated in :c:type:`peripheral_t`
+ */
 void adc_disable(peripheral_t adc);
+/***/
 void adc_set_mode(peripheral_t adc, adc_mode_t mode);
+/**
+ * Start the ADC conversion
+ *
+ * :param adc: a peripheral enumerated in :c:type:`peripheral_t`
+ */
 void adc_start(peripheral_t adc);
+/**
+ * Stop the ADC conversion
+ *
+ * :param adc: a peripheral enumerated in :c:type:`peripheral_t`
+ */
 void adc_stop(peripheral_t adc);
+/***/
 void adc_select_channel(peripheral_t adc, adc_channel_t channel);
+/***/
 void adc_set_trigger(peripheral_t adc, adc_trigger_t trigger);
+/***/
 uint32_t adc_read(peripheral_t adc);
+/***/
 void adc_enable_irq(peripheral_t adc);
+/***/
 void adc_disable_irq(peripheral_t adc);
+/***/
 adc_event_t adc_get_event(peripheral_t adc);
+/***/
 void adc_clear_event(peripheral_t adc, adc_event_t events);
+/***/
 bool adc_is_busy(peripheral_t adc);
-
+/***/
 void adc_set_clock_frequency(peripheral_t adc, uint32_t hz, uint32_t pclk);
 
 #if defined(__cplusplus)
