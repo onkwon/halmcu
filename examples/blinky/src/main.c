3195be3c5362bac2f15cd397c8ababa441a1a4f8
Kyunghwan Kwon <kwon@toanyone.net>
2021-04-21 16:27:20 +0900

    fix gpio example

--- examples/blinky/src/main.c
+++ examples/blinky/src/main.c
@@ -1,16 +1,17 @@
 #include "abov/hal/gpio.h"
 #include "abov/delay.h"
 
-#define LED_PIN			(GPIOD + 1)
+#define LED_PORT		PERI_GPIOD
+#define LED_PIN			1
 
 int main(void)
 {
-	gpio_open(LED_PIN, GPIO_MODE_PUSHPULL);
+	gpio_open(LED_PORT, LED_PIN, GPIO_MODE_PUSHPULL);
 
 	while (1) {
-		gpio_write(LED_PIN, 1);
+		gpio_write(LED_PORT, LED_PIN, 1);
 		udelay(500000);
-		gpio_write(LED_PIN, 0);
+		gpio_write(LED_PORT, LED_PIN, 0);
 		udelay(500000);
 	}
 
