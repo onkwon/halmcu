994917cf9bca4a9b6d009d14c24a590e8fb6014c
Kyunghwan Kwon <kwon@toanyone.net>
2021-05-19 08:14:36 +0900

    Refactor wdt

--- include/abov/ll/wdt.h
+++ include/abov/ll/wdt.h
@@ -15,10 +15,10 @@ extern "C" {
  *
  * This function makes WDT unit the reset state.
  */
-void wdt_reset(void);
+void wdt_ll_reset(void);
 /** Get the current watchdog count */
-uint32_t wdt_get_count(void);
-void wdt_set_reload_ms(uint32_t period_ms);
+uint32_t wdt_ll_get_count(void);
+void wdt_ll_set_reload_ms(uint32_t period_ms);
 
 /**
  * @brief Set watchdog interrupt
@@ -27,13 +27,22 @@ void wdt_set_reload_ms(uint32_t period_ms);
  *
  * @param[in] enable enables interrupt when true. disables when false
  */
-void wdt_set_interrupt(bool enable);
-bool wdt_is_event_raised(void);
+void wdt_ll_set_interrupt(bool enable);
+bool wdt_ll_is_event_raised(void);
+
+void wdt_ll_set_prescaler(uint32_t div_factor);
+uint32_t wdt_ll_get_prescaler(void);
+void wdt_ll_set_reload(uint32_t timeout);
+uint32_t wdt_ll_get_reload(void);
+void wdt_ll_feed(void);
+void wdt_ll_start(void);
+void wdt_ll_stop(void);
+void wdt_ll_set_debug_stop_mode(bool enable);
 
 #include "abov/ll/clk.h"
 
-void wdt_set_clock_source(clk_source_t clk);
-clk_source_t wdt_get_clock_source(void);
+void wdt_ll_set_clock_source(clk_source_t clk);
+clk_source_t wdt_ll_get_clock_source(void);
 
 #if defined(__cplusplus)
 }
--- tests/mocks/ll/wdt_mock.cpp
+++ tests/mocks/ll/wdt_mock.cpp
@@ -2,26 +2,53 @@
 
 #include "abov/ll/wdt.h"
 
-uint32_t wdt_get_prescaler(void)
+uint32_t wdt_ll_get_prescaler(void)
 {
 	return mock().actualCall(__func__)
 		.returnUnsignedIntValueOrDefault(1);
 }
 
-clk_source_t wdt_get_clock_source(void)
+clk_source_t wdt_ll_get_clock_source(void)
 {
 	return (clk_source_t)mock().actualCall(__func__)
 		.returnUnsignedIntValueOrDefault(CLK_PLL);
 }
 
-void wdt_set_reload(uint32_t timeout)
+void wdt_ll_set_reload(uint32_t timeout)
 {
 	mock().actualCall(__func__)
 		.withParameter("timeout", timeout);
 }
 
-uint32_t wdt_get_reload(void)
+uint32_t wdt_ll_get_reload(void)
 {
 	return mock().actualCall(__func__)
 		.returnUnsignedIntValueOrDefault(0);
+}
+
+void wdt_ll_set_prescaler(uint32_t div_factor)
+{
+	mock().actualCall(__func__)
+		.withParameter("div_factor", div_factor);
+}
+
+void wdt_ll_feed(void)
+{
+	mock().actualCall(__func__);
+}
+
+void wdt_ll_start(void)
+{
+	mock().actualCall(__func__);
+}
+
+void wdt_ll_stop(void)
+{
+	mock().actualCall(__func__);
+}
+
+void wdt_ll_set_debug_stop_mode(bool enable)
+{
+	mock().actualCall(__func__)
+		.withParameter("enable", enable);
 }
