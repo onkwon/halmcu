26c275817f51d5ed991586b202f192e5c628e7d9
Kyunghwan Kwon <kwon@toanyone.net>
2021-05-19 08:42:13 +0900

    Refactor gpio (#59)

--- hal/gpio.c
+++ hal/gpio.c
@@ -15,19 +15,19 @@ void gpio_open(periph_t port, uint32_t pin, const struct gpio_cfg *cfg)
 	pwr_enable_peripheral(port);
 	clk_enable_peripheral(port);
 
-	gpio_enable_port(port);
-	gpio_set_mode(port, pin, cfg->mode);
+	gpio_ll_enable_port(port);
+	gpio_ll_set_mode(port, pin, cfg->mode);
 
 	if (cfg->altfunc) {
-		gpio_set_altfunc(port, pin, cfg->altfunc_number);
+		gpio_ll_set_altfunc(port, pin, cfg->altfunc_number);
 	}
 
-	gpio_set_speed(port, pin, cfg->speed);
-	gpio_set_debouncer(port, pin, cfg->debounce);
+	gpio_ll_set_speed(port, pin, cfg->speed);
+	gpio_ll_set_debouncer(port, pin, cfg->debounce);
 
 	if (cfg->irq != 0) {
-		gpio_clear_event(port, pin);
-		gpio_enable_irq(port, pin, cfg->irq);
+		gpio_ll_clear_event(port, pin);
+		gpio_ll_enable_irq(port, pin, cfg->irq);
 		irq_enable(PERIPH_TO_IRQ(port));
 	}
 }
@@ -37,21 +37,21 @@ void gpio_open_output(periph_t port, uint32_t pin, gpio_mode_t mode)
 	pwr_enable_peripheral(port);
 	clk_enable_peripheral(port);
 
-	gpio_enable_port(port);
-	gpio_set_mode(port, pin, mode);
+	gpio_ll_enable_port(port);
+	gpio_ll_set_mode(port, pin, mode);
 }
 
 void gpio_close(periph_t port, uint32_t pin)
 {
-	gpio_disable_irq(port, pin);
+	gpio_ll_disable_irq(port, pin);
 	irq_disable(PERIPH_TO_IRQ(port));
 
-	gpio_set_mode(port, pin, GPIO_MODE_ANALOG);
-	gpio_set_debouncer(port, pin, 0); /* turn it off */
+	gpio_ll_set_mode(port, pin, GPIO_MODE_ANALOG);
+	gpio_ll_set_debouncer(port, pin, 0); /* turn it off */
 #if 0
-	if (gpio_get_active_pins(port) == 0) {
-		gpio_reset(port);
-		gpio_disable_port(port);
+	if (gpio_ll_get_active_pins(port) == 0) {
+		gpio_ll_reset(port);
+		gpio_ll_disable_port(port);
 	}
 #endif
 
--- include/abov/ll/gpio.h
+++ include/abov/ll/gpio.h
@@ -7,6 +7,21 @@ extern "C" {
 
 #include "abov/periph/gpio.h"
 
+void gpio_ll_reset(periph_t port);
+void gpio_ll_enable_port(periph_t port);
+void gpio_ll_disable_port(periph_t port);
+void gpio_ll_set_mode(periph_t port, uint32_t pin, gpio_mode_t mode);
+void gpio_ll_set_altfunc(periph_t port, uint32_t pin, int altfunc);
+void gpio_ll_set_speed(periph_t port, uint32_t pin, gpio_speed_t speed);
+void gpio_ll_set_debouncer(periph_t port, uint32_t pin, uint32_t pclk_clocks);
+void gpio_ll_enable_irq(periph_t port, uint32_t pin, gpio_irq_t irq_type);
+void gpio_ll_disable_irq(periph_t port, uint32_t pin);
+void gpio_ll_clear_event(periph_t port, uint32_t pin);
+void gpio_ll_write_port(periph_t port, int value);
+int gpio_ll_read_port(periph_t port);
+void gpio_ll_write(periph_t port, uint32_t pin, int value);
+int gpio_ll_read(periph_t port, uint32_t pin);
+
 #if defined(__cplusplus)
 }
 #endif
